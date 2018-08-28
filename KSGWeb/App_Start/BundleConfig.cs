c6666ea8ed3508222f079ee9a66351d22f811b74
김승기 <ksg6689@playworks.co.kr>
2018-08-28 10:36:48 +0900

    Project commit

---
+++ KSGWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace KSGWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Component/jquery/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+           /* bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));*/
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Component/Bootstrap/js/bootstrap.js",
+                      "~/Component/Bootstrap/js/poper.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Compoent/css").Include(
+                      "~/Component/Bootstrap/css/bootstrap.css",
+                      "~/Component/Site/site.css"
+                      ));
+
+            
+        }
+    }
+}
